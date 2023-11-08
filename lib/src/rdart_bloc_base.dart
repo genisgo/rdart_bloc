69d9bd2e0875cdc2c9de3bddf9f8849478d61ef4
genisgo <adamasogodogo.as@gmail.com>
2023-11-08 13:35:13 +0000

    update

--- lib/src/rdart_bloc_base.dart
+++ lib/src/rdart_bloc_base.dart
@@ -6,16 +6,23 @@ import 'package:rdart/rviews.dart';
 class BlocBuilder<B extends Bloc, S> extends Rview {
   final B bloc;
   final Relement Function(S state) builder;
-  BlocBuilder({required this.bloc, required this.builder});
+  final Function(S state)? onBuild;
+  BlocBuilder({required this.bloc, required this.builder, this.onBuild});
   late StreamSubscription<dynamic> _streamSubscription;
   Relement _currentview = SizeBox();
   @override
   void initState() {
+    //  _currentview = builder.call(event);
     _streamSubscription = bloc.stream.listen((event) {
       _currentview = builder.call(event);
       setState(() {});
     });
     super.initState();
+  }
+
+  @override
+  void onUpdate(old) {
+    onBuild?.call(bloc.state);
   }
 
   @override
